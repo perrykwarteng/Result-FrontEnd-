90c21536a219ff40198217c217237d6472d7d96e
Perry Kwarteng <perrykwarteng8@gmail.com>
2023-04-15 05:55:05 +0100

    added admin dashboard and a temp dashboard for all dashboards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminDashboardComponent } from './Admin/admin-dashboard/admin-dashboard.component';
+import { DashboardInterfaceComponent } from './dashboard-interface/dashboard-interface.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AdminDashboardComponent, DashboardInterfaceComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/dashboard-interface/dashboard-interface.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-interface',
+  templateUrl: './dashboard-interface.component.html',
+  styleUrls: ['./dashboard-interface.component.css']
+})
+export class DashboardInterfaceComponent {
+
+}
