6f6b8c841618e92b667417e6aae410232276d69a
Perry Kwarteng <perrykwarteng8@gmail.com>
2023-05-19 05:12:38 +0100

    created the ui for veiwing the list of teachers

---
+++ src/app/interfaces/teacher.ts
@@ -0,0 +1,11 @@
+export interface Teacher {
+    id:number
+    t_index:string
+    first_name:string
+    other_name:string
+    last_name:string
+    email:string
+    number:number
+    dob:Date
+    location:string
+}
